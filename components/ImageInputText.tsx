bb29403dea0a1c22cc7617a34ed17c3d0fd1378e
MGuerra <m.guerra1705@gmail.com>
2023-05-11 14:50:10 -0500

    add responsive text

--- components/ImageInputText.tsx
+++ components/ImageInputText.tsx
@@ -7,8 +7,13 @@ export default function ImageInputText({
 }) {
   if (isDragActive) return <p className='text-center'>Soltar aqui</p>
   return (
-    <p className='text-center'>
-      {image === '' ? 'Seleccionar o arrastrar imagen' : 'Modificar imagen'}
-    </p>
+    <>
+      <p className='text-center text-ellipsis whitespace-nowrap hidden lg:block'>
+        {image === '' ? 'Seleccionar o arrastrar imagen' : 'Modificar imagen'}
+      </p>
+      <p className='text-center text-ellipsis whitespace-nowrap lg:hidden'>
+        {image === '' ? 'Seleccionar imagen' : 'Modificar imagen'}
+      </p>
+    </>
   )
 }
