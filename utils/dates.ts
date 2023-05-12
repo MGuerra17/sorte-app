d2db9495e62df33251ec8db373e3d98c84e9da41
MGuerra <m.guerra1705@gmail.com>
2023-05-11 21:00:35 -0500

    Create Date from state

--- utils/dates.ts
+++ utils/dates.ts
@@ -15,8 +15,9 @@ const months = [
 
 export const formatDate = (date: Date | null) => {
   if (date == null) return ''
-  const day = date.getUTCDate()
-  const month = months[date.getUTCMonth()]
-  const year = date.getUTCFullYear()
+  const currentDate = new Date(date)
+  const day = currentDate.getUTCDate()
+  const month = months[currentDate.getUTCMonth()]
+  const year = currentDate.getUTCFullYear()
   return `${day} de ${month} del ${year}`
 }
