97487c6d657a7b2348784da731885d2d717adfa1
MGuerra <m.guerra1705@gmail.com>
2023-05-11 19:00:45 -0500

    Add max input max length

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,14 +1,19 @@
+import useHydrated from '@/hooks/useHydrated'
 import {
   Box,
+  Flex,
   FormLabel,
   Input,
   InputGroup,
-  InputLeftAddon
+  InputLeftAddon,
+  Text
 } from '@chakra-ui/react'
 
 interface TextInputProps {
   titulo: string
   placeholder: string
+  maxLength: number
+  type?: string
   leftAddon?: string
   value?: string
   setter: (value: string) => void
@@ -18,23 +23,32 @@ interface TextInputProps {
 export default function TextInput({
   titulo,
   placeholder,
+  maxLength,
+  type = 'text',
   value = '',
   leftAddon,
   setter,
   required = false
 }: TextInputProps) {
+  const { isHydrated } = useHydrated()
   return (
     <Box width='100%'>
-      <FormLabel>
-        {titulo}
-        {required ? <span className='text-red-600'> *</span> : ''}
-      </FormLabel>
+      <Flex justify='space-between' alignItems='end'>
+        <FormLabel>
+          {titulo}
+          {required ? <span className='text-red-600'> *</span> : ''}
+        </FormLabel>
+        <Text className='text-xs pb-1 text-[#51555E]'>
+          {isHydrated ? value.length : 0}/{maxLength}
+        </Text>
+      </Flex>
       <InputGroup>
         {leftAddon != null && <InputLeftAddon>{leftAddon}</InputLeftAddon>}
         <Input
+          maxLength={maxLength}
           focusBorderColor='green.400'
           placeholder={placeholder}
-          type='text'
+          type={type}
           value={value}
           onChange={(e) => {
             setter(e.target.value)
