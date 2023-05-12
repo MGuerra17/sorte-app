1a14f90d79d1b77b3b9a97301ddd0519df3b6967
MGuerra <m.guerra1705@gmail.com>
2023-05-12 00:32:55 -0500

    Create initial state

---
+++ utils/globalState.ts
@@ -0,0 +1,16 @@
+export const initialState = {
+  title: '',
+  description: '',
+  price: '',
+  image: '',
+  date: null,
+  phone: '',
+  winCondition: '',
+  responsible: '',
+  payment: [],
+  backgroundColor: 'white',
+  primaryColor: 'sky-300',
+  textColor: 'black',
+  numbersColor: 'orange-400',
+  soldNumbers: []
+}
