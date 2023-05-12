7d42a9f91aa7c79f30dd0eee3b0ca1e753007b20
MGuerra <m.guerra1705@gmail.com>
2023-05-11 20:57:33 -0500

    Add github link

--- components/PanelHeader.tsx
+++ components/PanelHeader.tsx
@@ -1,15 +1,25 @@
 import { Box, Flex, Heading } from '@chakra-ui/react'
 import Logo from './Logo'
 import DarkModeToggler from './DarkModeToggler'
+import GithubIcon from './icons/GithubIcon'
 
 export default function PanelHeader() {
   return (
     <Box>
       <Flex justify='space-between' alignItems='center'>
         <Logo />
-        <DarkModeToggler />
+        <Flex justify='center' alignItems='center' gap={2}>
+          <a
+            href='https://github.com/MGuerra17/sorte-app'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <GithubIcon width={26} height={26} />
+          </a>
+          <DarkModeToggler />
+        </Flex>
       </Flex>
-      <Heading className='mt-3 mb-3'>Crear rifa</Heading>
+      <Heading className='mt-3'>Crear rifa</Heading>
     </Box>
   )
 }
