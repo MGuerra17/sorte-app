ab3accf8f5660b3dfea0be69525a277ddd14550f
MGuerra <m.guerra1705@gmail.com>
2023-05-14 01:07:54 -0500

    Remove github link

--- components/PanelHeader.tsx
+++ components/PanelHeader.tsx
@@ -1,7 +1,6 @@
 import { Box, Flex, Heading } from '@chakra-ui/react'
 import Logo from './Logo'
 import DarkModeToggler from './DarkModeToggler'
-import GithubIcon from './icons/GithubIcon'
 import ResetRaffleButton from './ResetRaffleButton'
 
 export default function PanelHeader() {
@@ -9,16 +8,7 @@ export default function PanelHeader() {
     <Box>
       <Flex justify='space-between' alignItems='center' marginBottom={3}>
         <Logo />
-        <Flex justify='center' alignItems='center' gap={2}>
-          <a
-            href='https://github.com/MGuerra17/sorte-app'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <GithubIcon width={26} height={26} />
-          </a>
-          <DarkModeToggler />
-        </Flex>
+        <DarkModeToggler />
       </Flex>
       <Flex justify='space-between' alignItems='center'>
         <Heading>Crear rifa</Heading>
